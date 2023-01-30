a8034f032695accf9d3886fe999482f6283fb495
박상현 <cmk051@coredax.com>
2023-01-30 21:34:08 +0900

    제네릭 마무리

--- example/8_generics.ts
+++ example/8_generics.ts
@@ -12,24 +12,22 @@ function getValue<T>(value: T): T {
 }
 // 제네릭이란 호출할때 리턴값 및 인자값이 정해진다.
 
-getValue('hi').toLocaleUpperCase();
+getValue("hi").toLocaleUpperCase();
 getValue(100).toLocaleString();
 
-function logText(text:string | number) {
-  console.log(text)
+function logText(text: string | number) {
+  console.log(text);
   // string 이나 number 타입으로 매개변수를 받아도 타입이 확고하지 않으면 split 함수를 사용 못한다.
-  text.split()
+  text.split();
   return text;
 }
-
-
 
 // 제네릭 기본 문법 - 인터페이스
 interface Developer<T> {
   name: string;
   age: T;
 }
-const tony: Developer<number> = { name: 'tony', age: 100 };
+const tony: Developer<number> = { name: "tony", age: 100 };
 
 // 제네릭 타입 제한 - 구체적인 타입
 function getNumberAndArray<T>(value: T): T {
@@ -42,6 +40,20 @@ function getNumberAndArray<T>(value: T[]): T[] {
   return value;
 }
 
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface lengthType {
+  length: number;
+}
+// lengthType 인터페이스에 있는 length를 사용한다라는 의미
+function logTextLength<T extends lengthType>(text: T): T {
+  text.length;
+  return text;
+}
+// number type에서는 length를 지원하지 않는다.
+logTextLength(10);
+// string type에서는 length를 지원한다.
+logTextLength("asd");
+
 // 제네릭 타입 제한 - keyof
 interface ShoppingItems {
   name: string;
@@ -49,16 +61,18 @@ interface ShoppingItems {
   address: string;
   stock: number;
 }
+// ShoppingItems interface 에 사용할 수 있는 key를 제한할 수 있다.
 function getAllowedOptions<T extends keyof ShoppingItems>(option: T): T {
-  if (option === 'name' || option === 'address') {
-    console.log('option type is string');
+  if (option === "name" || option === "address") {
+    console.log("option type is string");
     return option;
   }
-  if (option === 'price' || option === 'stock') {
-    console.log('option type is number');
+  if (option === "price" || option === "stock") {
+    console.log("option type is number");
     return option;
   }
+  return option;
 }
-getAllowedOptions('nothing');
+getAllowedOptions("address");
 // const a = getAllowedOptions('name');
 // a.toUpperCase(); // Name
--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,17 +1,49 @@
-const emails = [
-  { value: 'naver.com', selected: true },
-  { value: 'gmail.com', selected: false },
-  { value: 'hanmail.net', selected: false },
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
+
+// interface 로 이용한 union 선언
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+const emails: DropdownItem<string>[] = [
+  { value: "naver.com", selected: true },
+  { value: "gmail.com", selected: false },
+  { value: "hanmail.net", selected: false },
 ];
 
-const numberOfProducts = [
+// const emails: Email[] = [
+//   { value: "naver.com", selected: true },
+//   { value: "gmail.com", selected: false },
+//   { value: "hanmail.net", selected: false },
+// ];
+
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
+
+// interface TrueFalse {
+//   value: boolean;
+//   selected: boolean;
+// }
+
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
-  const option = document.createElement('option');
+// const numberOfProducts: ProductNumber[] = [
+//   { value: 1, selected: true },
+//   { value: 2, selected: false },
+//   { value: 3, selected: false },
+// ];
+
+function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
+  const option = document.createElement("option");
   option.value = item.value.toString();
   option.innerText = item.value.toString();
   option.selected = item.selected;
@@ -21,6 +53,10 @@ function createDropdownItem(item) {
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
   const item = createDropdownItem(email);
-  const selectTag = document.querySelector('#email-dropdown');
+  const selectTag = document.querySelector("#email-dropdown");
   selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem(product);
 });
